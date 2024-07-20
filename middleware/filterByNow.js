cb3b4be60f68b784a2d58d6068e143760d173919
AL1Skey <adamnurramadan@gmail.com>
2024-07-20 10:23:27 +0700

    [FIX] filterByNow

--- middleware/filterByNow.js
+++ middleware/filterByNow.js
@@ -2,7 +2,8 @@ const filterByNow = (req, res, next) => {
     const dateStr = new Date().toISOString().slice(0, 10);
     let date = new Date(dateStr);
     date = date.toLocaleDateString("id-ID", { weekday: 'long' }); 
-    req.user.date = date;
+    console.log(date);
+    req.user = {...req.user, date};
     next();
 };
 
